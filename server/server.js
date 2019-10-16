dc06f6e0ee23aa0b2e2fcc005fb8f6bf10426da2
SanjeVicor <>
2019-10-16 14:04:40 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const socketIO = require('socket.io');
+const socketIO = require('socket.io'); 
 
 const app = express();
 
